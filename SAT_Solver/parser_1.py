da2e79dbd809317b47fffc6d567252dfe8a71593
mahmud624683 <mahmudul.hasan.bueteee@gmail.com>
2024-06-27 11:18:23 +0600

    new branch mahmudul added to head for developing a different approach for sat solver

---
+++ SAT_Solver/parser_1.py
@@ -0,0 +1,58 @@
+import re
+
+def parse_verilog(verilog_code):
+    lines = verilog_code.split('\n')
+    gates = {}
+    inputs = []
+    outputs = []
+    
+    for line in lines:
+        line = line.strip()
+        if line.startswith("INPUT"):
+            inputs.append(re.findall(r'\((.*?)\)', line)[0])
+        elif line.startswith("OUTPUT"):
+            outputs.append(re.findall(r'\((.*?)\)', line)[0])
+        elif '=' in line:
+            left, right = line.split('=')
+            left = left.strip()
+            right = right.strip()
+            gates[left] = right
+    
+    return inputs, outputs, gates
+
+
+def get_expression(gates, inputs, target):
+    if target in gates:
+        expression = gates[target]
+        parts = re.findall(r'\((.*?)\)', expression)[0].split(',')
+        parts_len = len(parts)
+        for i in range(parts_len):
+            target_gate = parts[i].strip()
+            target_expression = get_expression(gates, inputs, parts[i].strip())
+            if target_expression != "":
+                expression = expression.replace(target_gate,target_expression)
+        return expression
+    elif target in inputs:
+        return target
+    
+    return ""
+
+def extract_expressions(verilog_file_path):
+    with open(verilog_file_path, 'r') as file:
+        verilog_code = file.read()
+    
+    inputs, outputs, gates = parse_verilog(verilog_code)
+    expressions = {}
+    
+    for output in outputs:
+        if output in gates:
+            expressions[output] = get_expression(gates, inputs, output)
+    
+    return expressions
+
+# Example usage
+verilog_file_path = 'E:\\Personal\\SAT_Solver\\verilog_file.v'
+expressions = extract_expressions(verilog_file_path)
+for output, expression in expressions.items():
+    print(f"{output} = {expression}")
+
